098a0a0c1e4e4bb3143e2a445a35b383cfffeb53
Jarvis <jarvismiranda@gmail.com>
2022-02-18 17:35:34 -0500

    haciendo url dinamicas para el SEO

--- components/Entrada.js
+++ components/Entrada.js
@@ -4,15 +4,15 @@ import { formatearFecha } from "../helpers/index";
 import styles from "../styles/Entrada.module.css";
 
 const Entrada = ({ entrada }) => {
-  const { titulo, resumen, imagen, published_at, id } = entrada;
+  const { titulo, resumen, imagen, published_at, id, url } = entrada;
   const urlImg = `${process.env.NEXT_PUBLIC_API_URL}${imagen.url}`;
-  console.log(imagen.formats.thumbnail.url);
   const fecha = formatearFecha(published_at);
   return (
     <article>
       <div className={styles.imagen}>
         <Image
           priority="true"
+          layout="responsive"
           src={urlImg}
           height={800}
           width={600}
@@ -23,7 +23,7 @@ const Entrada = ({ entrada }) => {
         <h3 className={styles.contenido}>{titulo}</h3>
         <p className={styles.fecha}>{fecha}</p>
         <p className={styles.resumen}>{resumen}</p>
-        <Link href={`/blog/${id}`}>
+        <Link href={`/blog/${url}`}>
           <a className={styles.enlace}>Leer Entrada</a>
         </Link>
       </div>
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,25 +1,67 @@
+import Image from "next/image";
 import Layout from "../../components/Layout";
+import { formatearFecha } from "../../helpers/index";
+import styles from "../../styles/Entrada.module.css";
+import NoEncontrado from "../404";
 
 const EntradaBlog = ({ entrada }) => {
-  const { titulo, imagen, published_at, id } = entrada;
+  const { contenido, imagen, published_at, titulo } = entrada;
+  const urlImg = `${process.env.NEXT_PUBLIC_API_URL}${imagen.url}`;
+  const fecha = formatearFecha(published_at);
+
   return (
-    <Layout>
-      <main className="contenedor">
-        <h1 className="heading">{entrada.titulo}</h1>
-      </main>
+    <Layout pagina={titulo}>
+      {contenido ? (
+        <main className="contenedor">
+          <h1 className="heading">{titulo}</h1>
+          <article className={styles.entrada}>
+            <div>
+              <Image
+                priority="true"
+                layout="responsive"
+                src={urlImg}
+                height={800}
+                width={600}
+                alt={`imagen entrada ${titulo}`}
+              />
+            </div>
+            <div className={styles.contenido}>
+              <p className={styles.fecha}>{fecha}</p>
+              <p className={styles.texto}>{contenido}</p>
+            </div>
+          </article>
+        </main>
+      ) : (
+        <NoEncontrado />
+      )}
     </Layout>
   );
 };
 
 export async function getServerSideProps({ query }) {
-  const url = `${process.env.API_URL}/blogs/${query.id}`;
+  const url = `${process.env.API_URL}/blogs?url=${query.id}`;
   const resultado = await fetch(url);
   const entrada = await resultado.json();
-  return {
-    props: {
-      entrada,
-    },
-  };
+  if (entrada.length > 0) {
+    return {
+      props: {
+        entrada: entrada[0],
+      },
+    };
+  } else {
+    return {
+      props: {
+        entrada: {
+          contenido: "",
+          imagen: {
+            url: "",
+          },
+          published_at: "",
+          titulo: "",
+        },
+      },
+    };
+  }
 }
 
 export default EntradaBlog;
