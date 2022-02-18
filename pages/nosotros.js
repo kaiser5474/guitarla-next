130067a5311e7e8d47a5d3d8e70003e7e53e372a
Jarvis <jarvismiranda@gmail.com>
2022-02-18 00:31:06 -0500

    modificando pagina nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,12 +1,50 @@
 import Link from "next/link";
+import Image from "next/image";
 import Layout from "../components/Layout";
+import styles from "../styles/Nosotros.module.css";
 
 const Nosotros = () => {
   return (
     <>
       <Layout pagina={"Nosotros"}>
-        <Link href="/">Inicio</Link>
-        <div>Desde nosotros</div>
+        <main className="contenedor">
+          <h2 className="heading">Nosotros</h2>
+          <div className={styles.contenido}>
+            <Image
+              layout="responsive"
+              src="/img/nosotros.jpg"
+              height={600}
+              width={450}
+              alt="imagen sobre nosotros"
+            ></Image>
+            <div>
+              <p>
+                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+                printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it to make a
+                type specimen book. It has survived not only five centuries, but
+                also the leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.{" "}
+              </p>
+              <p>
+                What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+                printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an
+                unknown printer took a galley of type and scrambled it to make a
+                type specimen book. It has survived not only five centuries, but
+                also the leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.
+              </p>
+            </div>
+          </div>
+        </main>
       </Layout>
     </>
   );
